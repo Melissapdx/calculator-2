ef0374d34101e3c5e071e06b821f69fa2d0544b1
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-23 15:20:27 -0700

    working calculator

--- calculator.py
+++ calculator.py
@@ -21,20 +21,20 @@ while True:
     if tokens[0] == "q":
         break
     elif tokens[0] == "+":
-        print add(float(tokens[1]),float(tokens[2]))
+        print add(float(tokens[1]), float(tokens[2]))
     elif tokens[0] == "-":
-        print subtract(float(tokens[1]),float(tokens[2]))
+        print subtract(float(tokens[1]), float(tokens[2]))
     elif tokens[0] == "*":
-        print multiply(float(tokens[1]),float(tokens[2]))
+        print multiply(float(tokens[1]), float(tokens[2]))
     elif tokens[0] == "/":
-        print divide(float(tokens[1]),float(tokens[2]))
+        print divide(float(tokens[1]), float(tokens[2]))
     elif tokens[0] == "square":
         print square(float(tokens[1]))
     elif tokens[0] == "cube":
         print cube(float(tokens[1]))
     elif tokens[0] == "pow":
-        print power(float(tokens[1]),float(tokens[2]))
+        print power(float(tokens[1]), float(tokens[2]))
     elif tokens[0] == "mod":
-        print mod(float(tokens[1]),float(tokens[2]))
+        print mod(float(tokens[1]), float(tokens[2]))
     else:
         print "not a valid option"
